615e1fdea0dfb076938a9a5a67d5bd14f80a76dd
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-05-28 05:57:00 -0700

    0.0.2

--- lib/widgets/appbar/appbar.dart
+++ lib/widgets/appbar/appbar.dart
@@ -4,11 +4,12 @@
 
 import 'dart:ui';
 
-// Importing necessary widgets from the material package and a custom widget
+// Importing necessary  from the material package and a custom widget
 import 'package:flutter/material.dart';
+import 'package:smooth_widgets/extensions/darkmode.extension.dart';
 import 'package:smooth_widgets/widgets/buttons/icon.button.dart';
 
-// A stateless widget representing a custom app bar with smooth buttons
+// A stateless representing a custom app bar with smooth buttons
 class SmoothAppbar extends StatelessWidget {
   // Class properties, including the title and icons with their associated callbacks
   String? title;
@@ -17,6 +18,7 @@ class SmoothAppbar extends StatelessWidget {
   VoidCallback? leftIconOnPressed;
   IconData? rightIcon;
   VoidCallback? rightIconOnPressed;
+  PreferredSizeWidget? bottom;
 
   // Constructor for the class, using named parameters
   SmoothAppbar({
@@ -27,34 +29,35 @@ class SmoothAppbar extends StatelessWidget {
     this.rightIcon,
     this.leftIconOnPressed,
     this.rightIconOnPressed,
+    this.bottom,
   });
 
   // Build method to create the app bar widget
   @override
-  Widget build(BuildContext context) {
-    // Adding a directionality widget to ensure correct layout based on text direction
+  build(BuildContext context) {
+    // Adding a directionality to ensure correct layout based on text direction
     return Directionality(
       textDirection: TextDirection.rtl,
-      // Adding a clip rect widget to clip the backdrop filter
       child: ClipRect(
-        // Adding a backdrop filter widget to add a blur effect to the container
         child: BackdropFilter(
           filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 65.0),
-          // A container widget with padding and a border to hold the app bar
           child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            padding: EdgeInsets.symmetric(horizontal: 15.0),
             decoration: BoxDecoration(
               color: Colors.transparent,
               border: Border(
                 bottom: BorderSide(
                   width: 1,
-                  color: const Color(0xffF1F1F1).withOpacity(0.5),
+                  color: !context.isDarkMode
+                      ? const Color(0xffF1F1F1).withOpacity(0.5)
+                      : const Color(0xffF1F1F1).withOpacity(0.1),
                 ),
               ),
             ),
-            // Adding an app bar widget with a transparent background, smooth icons, and a title with a callback
             child: AppBar(
-              backgroundColor: Colors.white.withOpacity(0),
+              backgroundColor: !context.isDarkMode
+                  ? Colors.white.withOpacity(0)
+                  : Colors.black.withOpacity(0),
               leadingWidth: 50,
               leading: Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -69,11 +72,11 @@ class SmoothAppbar extends StatelessWidget {
               title: GestureDetector(
                 onTap: titleOnTap,
                 child: Text(
-                  "$title",
-                  style: const TextStyle(
+                  title!,
+                  style: TextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.w600,
-                    color: Colors.black,
+                    color: !context.isDarkMode ? Colors.black : Colors.white,
                   ),
                 ),
               ),
@@ -86,6 +89,7 @@ class SmoothAppbar extends StatelessWidget {
                   ),
                 ),
               ],
+              bottom: bottom,
             ),
           ),
         ),
